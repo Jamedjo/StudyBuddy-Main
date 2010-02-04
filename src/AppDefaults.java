175620207d2cf68f55952e9b9d26c6111c8aa0f5
Jamedjo <jamedjo@gmail.com>
2010-02-04 09:26:12 +0000

    Default settings now managed by AppDefaults.java.

---
+++ src/AppDefaults.java
@@ -0,0 +1,39 @@
+
+enum AppDefaults {
+
+    //study buddy version used? (if different reset defaults?
+    //seperate properties version, which should be incremented if defaults need to be reset?
+    //date settings changed?
+    //thumbnails directory
+    //might change this to an array so a loop can be used to set values. Or an enum.
+    s1("testDefault1", "testval1"),
+    s2("testDefault2", "22222"),
+    s3("testDefault3", "/thumbnaildir/small/test/");//must deal with OS dependant path seperator
+
+
+    String key, value;
+
+    AppDefaults(String k, String v) {
+        key = k;
+        value = v;
+    }
+
+    static void set(Settings settings) {
+        for (AppDefaults setting : AppDefaults.values()) {
+            settings.setSettingDontSave(setting.key, setting.value);
+        }
+        settings.saveSettings();
+    }
+
+    static void getAndPrint(Settings settings){
+        for (AppDefaults setting : AppDefaults.values()) {
+            System.out.println("Default key: "+setting.key+" has value: "+settings.getSetting(setting.key));
+        }        
+    }
+
+    public static void main(String[] args) {
+        for (AppDefaults setting : AppDefaults.values()) {
+            System.out.println(setting.key + " :key||| <-     -> |||value:" + setting.value);
+        }
+    }
+}
--- src/Settings.java
+++ src/Settings.java
@@ -11,16 +11,7 @@ public class Settings {
     private final String propertiesFile = "StudyBuddy.properties";//Name of the file use to store properties
     private File propFile;
     private Properties javaProperties;
-
-    //Default values
-    private final String testDefault1val = "testval1";
-    private final String testDefault2val = "22222";
-    private final String testDefault3val = "/thumbnaildir/small/test/";//must deal with OS dependant path seperator
-    //study buddy version used? (if different reset defaults?
-    //seperate properties version, which should be incremented if defaults need to be reset?
-    //date settings changed?
-    //thumbnails directory
-    //might change this to an array so a loop can be used to set values. Or an enum.
+    //Default setting values moved to AppDefaults.java and controlled by an enum.
 
     Settings(){
         appPath = (System.getProperty("user.home"));
@@ -83,9 +74,7 @@ public class Settings {
 
     private void setDefaults(){
         setSettingDontSave("homeDir",appPath);
-        setSettingDontSave("testDefault1",testDefault1val);
-        setSettingDontSave("testDefault2",testDefault2val);
-        setSettingAndSave("testDefault3",testDefault3val);
+        AppDefaults.set(this);
     }
 
     //Use this when setting many settings. Use setSettingAndSave for final one.
@@ -118,23 +107,21 @@ public class Settings {
 
     public static void main(String[] args){
         Settings props = new Settings();
-        System.out.println("(private) StudyBudy folder is:\n"+props.appPath);
-        System.out.println("\n(public) getSetting finds StudyBudy folder as:\n"+props.getSetting("homeDir"));
-        props.setSettingAndSave("testSaveAllSettigngsHere", "settingsvalue");//Test setting a property in the file and saving all settings to file
-        props.setSettingDontSave("numberOfThumbnails", "5");//Test setting a property in the file. Doesn't save Properties to file.
-        System.out.println("\n test property set at '5' has value: "+props.getSetting("numberOfThumnails"));
-        System.out.println("default property test value 1: "+props.getSetting("testDefault1"));
-        System.out.println("default property test value 2: "+props.getSetting("testDefault2"));
-        System.out.println("default property test value 3: "+props.getSetting("testDefault3"));
         System.out.println("Os is windows?..."+props.isWindows());
+        System.out.println("(private) StudyBudy folder is: "+props.appPath);
+        System.out.println("(public) getSetting finds StudyBudy folder as: "+props.getSetting("homeDir"));
+        props.setSettingAndSave("testSaveAllSettigngsHere", "settingsvalue");//Test setting a property in the file and saving all settings to file
+        AppDefaults.getAndPrint(props);
+        props.setSettingDontSave("numberOfThumbnails", "5");//Test setting a property in the file. Doesn't save Properties to file.
+        System.out.println("Test property set at '5' has value: "+props.getSetting("numberOfThumbnails"));
         props = null;
 
         //Simulate closing StudyBudy and running a second time
 
         //Any settings which are not saved will no longer exist and return null
         props = new Settings();
-        System.out.println("default property test value 3: "+props.getSetting("testDefault3"));
-        System.out.println("test property set at '5' has value: "+props.getSetting("numberOfThumnails"));
+        System.out.println("\nFirst default setting has value: "+props.getSetting(AppDefaults.s1.key));
+        System.out.println("(unsaved) Test property set at '5' has value: "+props.getSetting("numberOfThumbnails"));
 
     }
 }
