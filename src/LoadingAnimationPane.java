b0c0d62c78ec277e7d0e43aed4eb034ffa5d263b
Jamedjo <jamedjo@gmail.com>
2010-03-24 00:04:21 +0000

    Fixed size, posistioning and updating of loading animation.

--- src/LoadingAnimationPane.java
+++ src/LoadingAnimationPane.java
@@ -5,14 +5,13 @@ import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
-import javax.swing.JViewport;
 
 /*
  *
  * This class is a JPanel which draws the loading animation in the middle
  *
  */
-public class LoadingAnimationPane extends JPanel {//Make into enum with overrides- would allow same class to be used for other icons and animations
+public class LoadingAnimationPane extends JPanel {//Use enum with switch statements- would allow same class to be used for other icons and animations
 
     BufferedImage blank = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
     private boolean shouldRepaint = false;
@@ -43,20 +42,28 @@ public class LoadingAnimationPane extends JPanel {//Make into enum with override
 
     void updatetAffine(){
         angle+=Math.toRadians(10);
-        //onResize();
+        onResize();
     }
 
     void onResize(){
         if(useParentSize) setPreferredSize(getParent().getPreferredSize());
-        int leftLoadOS = ((JViewport) getParent().getParent()).getViewPosition().x;
-        int topLoadOS = ((JViewport) getParent().getParent()).getViewPosition().y;
 
-        int anchorX=(getWidth()/2)-leftLoadOS;
-        int anchorY=(getHeight()/2)-topLoadOS;
+        int anchorX=(getPaneWidth()/2);
+        int anchorY=(getPaneHeight()/2);
 
         affine= new AffineTransform(((Graphics2D)getGraphics()).getTransform());
         affine.rotate(angle,anchorX, anchorY);
         repaint();
+    }
+
+    public int getPaneWidth(){
+        if(useParentSize) return getParent().getParent().getWidth();
+        return getWidth();
+    }
+
+    public int getPaneHeight(){
+        if(useParentSize) return getParent().getParent().getHeight();
+        return getHeight();
     }
 
     @Override
@@ -64,16 +71,14 @@ public class LoadingAnimationPane extends JPanel {//Make into enum with override
         super.paintComponent(g);
         if (shouldRepaint) {
             Graphics2D g2 = (Graphics2D) g;
-            //AffineTransform originalAffine = g2.getTransform();
             g2.setTransform(affine);
             g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
             currentImage = ErrorImages.getLoading();
-            loadingWH = ImageUtils.scaleToMax(currentImage.getWidth(), currentImage.getHeight(), getWidth(), getHeight());
-            int leftOffset=((getWidth() - ((int)(loadingWH.width/scale))) / 2);
-            int topOffset=((getHeight() - ((int)(loadingWH.height/scale))) / 2);
-            int leftLoadOS = ((JViewport) getParent().getParent()).getViewPosition().x;
-            int topLoadOS =  ((JViewport) getParent().getParent()).getViewPosition().y;
-            g2.drawImage(currentImage,leftOffset-leftLoadOS,topOffset-topLoadOS, ((int)(loadingWH.width/scale)), ((int)(loadingWH.height/scale)), this);
+            loadingWH = ImageUtils.scaleToMax(currentImage.getWidth(), currentImage.getHeight(), getPaneWidth(), getPaneHeight());
+            int leftOffset=((getPaneWidth() - ((int)(loadingWH.width/scale))) / 2);
+            int topOffset=((getPaneHeight() - ((int)(loadingWH.height/scale))) / 2);
+            g2.drawImage(currentImage,leftOffset,topOffset, ((int)(loadingWH.width/scale)), ((int)(loadingWH.height/scale)), this);
         }
         //g2.dispose();?
     }
